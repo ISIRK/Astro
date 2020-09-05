7de2bb11abab07329e718c66c426356c3f8a9ebf
Isaac Sirkin <isaac@my-mbp.local>
2020-09-04 19:18:54 -0600

    Changes

--- Astro/Astro.py
+++ Astro/Astro.py
@@ -18,9 +18,9 @@ async def on_message(message):
 
         helpembed = discord.Embed(title="Help", description="**Astro Bot** \n A Utilities Discord Bot with reliability and simplicity\n Made By isirk#0001", color=0x7289DA)
         helpembed.add_field(name= ":gear: Updates", value="Version 0.2 \n :inbox_tray: Finished help command \n :outbox_tray: Making Kick Command", inline=True)
-        helpembed.add_field(name= ":link: Links", value="[Bot Site](https://asksirk.com/Astro) \n [Patreon](https://www.patreon.com/Astro_Bot)")
+        helpembed.add_field(name= ":link: Links", value="[Bot Site](https://asksirk.com/Astro) \n [Github Repository](https://github.com/ISIRK/Astro) \n [Patreon](https://www.patreon.com/Astro_Bot)")
         helpembed.set_footer(text="Astro Bot | discord.gg/7yZqHfG")
 
         await message.channel.send(embed=helpembed)
 
-client.run('')
+client.run('NzUxNDQ3OTk1MjcwMTY4NTg2.X1JOew.AU7LaGLfrdE6eNoZOCapbnAG-Gg')
