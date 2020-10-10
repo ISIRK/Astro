dfc27d962356e6aa849783da535798c8226b0eba
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-10 08:47:05 -0600

    Update mod.py

--- Astro/cogs/mod.py
+++ Astro/cogs/mod.py
@@ -96,6 +96,9 @@ class Mod(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def warn(self , ctx, user : discord.Member, *, reason):
         '''Warn a Member'''
+        if user == self.bot.get_user(296862365503193098): #isirk is: 542405601255489537
+            await ctx.send("No Way Jose")
+        else:
             guild = ctx.guild
             embed = discord.Embed(color=0x7289DA)
             embed.set_author(name=f"Warned By {ctx.author}", icon_url=ctx.author.avatar_url)
