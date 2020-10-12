cff4a7e8d85d6ece7044247864a0cf8fe8ab991d
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-12 11:06:50 -0600

    Update utility.py

--- Astro/cogs/utility.py
+++ Astro/cogs/utility.py
@@ -110,11 +110,10 @@ class utility(commands.Cog):
         pingembed.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         pingembed.add_field(name="Pong!", value=f'```autohotkey\n{round(self.bot.latency * 1000)} ms```')
                                 start = time.perf_counter()
-            message = await ctx.send("Ping...")
-            end = time.perf_counter()
-            duration = (end - start) * 1000
-            await message.edit(content='Pong! {:.2f}ms'.format(duration))
-        pingembed.add_field(name="Typing", value=f"```autohotkey\n {:.2f}ms'.format(duration))
+                                message = await ctx.send("Ping...")
+                                end = time.perf_counter()
+                                duration = (end - start) * 1000
+        pingembed.add_field(name="Typing", value=f"```autohotkey\n{:.2f}ms'.format{duration})
         await ctx.send(embed=pingembed)
 
     @commands.command()
