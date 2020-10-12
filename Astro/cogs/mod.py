59de20178e567c7d7e2e5b7332f05f114d597fd5
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-12 08:40:42 -0600

    Update mod.py

--- Astro/cogs/mod.py
+++ Astro/cogs/mod.py
@@ -17,7 +17,7 @@ class Mod(commands.Cog):
             await ctx.send("You cannot kick yourself.")
         else:
             await user.kick()
-            embed = discord.Embed(title=f'User {user.name} has been kicked.', color=0x7289DA)
+            embed = discord.Embed(title=f'User {user.name} has been kicked.', color=0x2F3136)
             embed.add_field(name="Bai!", value=":wave:")
             embed.set_thumbnail(url=user.avatar_url)
             await ctx.send(embed=embed)
@@ -32,7 +32,7 @@ class Mod(commands.Cog):
             await ctx.send("You cannot ban yourself.")
         else:
             await user.ban()
-            embed = discord.Embed(title=f'User {user.name} has been banned.', color=0x7289DA)
+            embed = discord.Embed(title=f'User {user.name} has been banned.', color=0x2F3136)
             embed.add_field(name="Bai!", value=":hammer:")
             embed.set_thumbnail(url=user.avatar_url)
             await ctx.send(embed=embed)
@@ -46,12 +46,12 @@ class Mod(commands.Cog):
         else:
             rolem = discord.utils.get(ctx.message.guild.roles, name='Muted')
             if rolem is None:
-                embed=discord.Embed(title="Muted role", url="http://echo-bot.wikia.com/wiki/Setting_up_the_muted_role", description="The mute command requires a role named 'Muted'.", color=0x7289DA)
+                embed=discord.Embed(title="Muted role", url="http://echo-bot.wikia.com/wiki/Setting_up_the_muted_role", description="The mute command requires a role named 'Muted'.", color=0x2F3136)
                 embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
                 embed.set_footer(text="Without this role, the command will not work.")
                 await ctx.send(embed=embed)
             elif rolem not in user.roles:
-                embed = discord.Embed(title=f'User {user.name} has been successfully muted for {time}s.', color=0x7289DA)
+                embed = discord.Embed(title=f'User {user.name} has been successfully muted for {time}s.', color=0x2F3136)
                 embed.add_field(name="Shhh!", value=":zipper_mouth:")
                 embed.set_thumbnail(url=user.avatar_url)
                 await ctx.send(embed=embed)
@@ -60,7 +60,7 @@ class Mod(commands.Cog):
                 if rolem in user.roles:
                     try:
                         await user.remove_roles(rolem)
-                        embed = discord.Embed(title=f'User {user.name} has been automatically unmuted.', color=0x7289DA)
+                        embed = discord.Embed(title=f'User {user.name} has been automatically unmuted.', color=0x2F3136)
                         embed.add_field(name="Welcome back!", value=":open_mouth:")
                         embed.set_thumbnail(url=user.avatar_url)
                         await ctx.send(embed=embed)
@@ -75,7 +75,7 @@ class Mod(commands.Cog):
         """Unmutes a user."""
         rolem = discord.utils.get(ctx.message.guild.roles, name='Muted')
         if rolem in user.roles:
-            embed = discord.Embed(title=f'User {user.name} has been manually unmuted.', color=0x7289DA)
+            embed = discord.Embed(title=f'User {user.name} has been manually unmuted.', color=0x2F3136)
             embed.add_field(name="Welcome back!", value=":open_mouth:")
             embed.set_thumbnail(url=user.avatar_url)
             await ctx.send(embed=embed)
@@ -112,7 +112,7 @@ class Mod(commands.Cog):
             await ctx.send("Reason is a required argument that is missing.")
         else:
             guild = ctx.guild
-            embed = discord.Embed(color=0x7289DA)
+            embed = discord.Embed(color=0x2F3136)
             embed.set_author(name=f"Warned By {ctx.author}", icon_url=ctx.author.avatar_url)
             embed.add_field(name=f"You Have Been Warned in {guild}\n\nReason:", value=f'{reason}')
             embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/758453150897799172.png?v=1")
