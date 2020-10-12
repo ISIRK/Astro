9550b9db9a6b135b685d86bfb5f7e11db4a1ca97
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-12 10:35:57 -0600

    Update info.py

--- Astro/cogs/info.py
+++ Astro/cogs/info.py
@@ -41,7 +41,7 @@ class info(commands.Cog):
     @commands.command()
     async def invite(self , ctx):
         '''Get the invite for the bot.'''
-        embed = discord.Embed(title="Invite", description="[Invite Link](https://discord.com/oauth2/authorize?client_id=751447995270168586&permissions=268823638&redirect_uri=https%3A%2F%2Fastrobot.carrd.co%2F&response_type=code&scope=bot%20identify)", color=0x2F3136)
+        embed = discord.Embed(title="Invite", description="🌐 [Invite Link](https://discord.com/oauth2/authorize?client_id=751447995270168586&permissions=268823638&redirect_uri=https%3A%2F%2Fastrobot.carrd.co%2F&response_type=code&scope=bot%20identify)", color=0x2F3136)
         embed.add_field(name="Contact", value="To contact either dm @isirk#0001 or\nJoin Our [Discord Server](https://discord.gg/7yZqHfG)\n\nhttps://discord.gg/7yZqHfG", inline=False)
         embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/751447995270168586/103a763a727a393b9357df35102a638d.webp?size=1024")
         await ctx.send(embed=embed)
