b48a305d51b69d5ea44cc01698f6d33af5776ef5
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-14 08:28:19 -0600

    Update utility.py

--- Astro/cogs/utility.py
+++ Astro/cogs/utility.py
@@ -106,7 +106,7 @@ class utility(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         '''Get the bot ping'''                        
-        pingembed = discord.Embed(color=0x2F3136)
+        pingembed = discord.Embed(title="Pong!", color=0x2F3136)
         pingembed.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         pingembed.add_field(name="Server", value=f'```autohotkey\n{round(self.bot.latency * 1000)} ms```')
                         
