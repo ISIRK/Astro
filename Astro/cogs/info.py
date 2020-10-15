bba74d327ecdbaab3837866cc712b184ec288e97
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-15 09:04:23 -0600

    Update info.py

--- Astro/cogs/info.py
+++ Astro/cogs/info.py
@@ -72,12 +72,18 @@ class info(commands.Cog):
         mod.set_footer(text="Astro Bot | discord.gg/7yZqHfG")
         mod.add_field(name="Syntax:", value="```yaml\n<input> - Input for the Command\n(permission) - Required Permissions```", inline=False)
         mod.add_field(name="Commands:", value="`kick <user>` - Kick the mentioned user from the server. (`kick_members`)\n`ban <user>` - Ban the mentioned user from the server. (`ban_members`)\n`warn <user> <reason>` - Warn A User for a Specified Reason.(`kick_members`)\n`clear <#>` - Delete a number of messages. **Max 100** (`manage_messages`)\n`slowmode <seconds>` - Change the Channel Slowmode to a specified number of seconds. (`manage_channels`)\n~~~`mute <user>` - Mute a User.~~~ (Re-Working)\n~~~`unmute <user>` - Un-mutes a User.~~~ (Re-Working)", inline=False)
-
+        
+        other = discord.Embed(title="Other", description="Other Commands", color=0x2F3136)
+        other.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+        other.set_footer(text="Astro Bot | discord.gg/7yZqHfG")
+        other.add_field(name="Commands:", value="`dice` - Roll A Dice (*There is a possibility you'll get a surprise*)", inline=False)
+        
         embeds = [
             helpembed1,
             info,
             utility,
             mod,
+            other,
             Embed(title="End!", description="More to come...", color=discord.Colour.blurple())
         ]
 
