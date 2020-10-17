192ee34d25f289ed2ba1b8f2857cf05614b3aa67
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-17 13:48:36 -0600

    Update info.py

--- Astro/cogs/info.py
+++ Astro/cogs/info.py
@@ -1,14 +1,20 @@
-from multiprocessing.connection import Client
 import discord
+
 from discord import Embed
-from discord.ext import commands
-from discord.shard import ShardInfo
 from discord.user import User
 from discord.utils import get
-from datetime import datetime
+from discord.ext import commands
+from discord.shard import ShardInfo
+
 import os
+
 import collections
+
 import time, datetime
+from datetime import datetime
+
+from multiprocessing.connection import Client
+
 from disputils import BotEmbedPaginator, BotConfirmation, BotMultipleChoice
 
 class info(commands.Cog):
