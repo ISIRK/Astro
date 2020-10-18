402cb90082651f6b79cfe99559735043eedefcd5
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-18 13:38:01 -0600

    Update utility.py

--- Astro/cogs/utility.py
+++ Astro/cogs/utility.py
@@ -118,7 +118,7 @@ class utility(commands.Cog):
                         
         pingembed.add_field(name="<a:typing:765946280601059349> Typing", value='```autohotkey\n{:.2f} ms```'.format(duration))
                         
-        await message.edit(content=f"{embed=pingembed}")
+        await message.edit(content=f"{pingembed}")
 
     @commands.command()
     async def stats(self ,ctx):
