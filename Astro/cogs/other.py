3b8130eeb2cc793459032e37d8c5df8a5f3d7139
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-18 14:06:19 -0600

    Update other.py

--- Astro/cogs/other.py
+++ Astro/cogs/other.py
@@ -52,7 +52,7 @@ class other(commands.Cog):
         
     @commands.command()
     @commands.cooldown(1,10,BucketType.user)
-    async def binary(self, ctx, text: str):
+    async def binary(self, ctx, *, text: str):
         async with aiohttp.ClientSession() as session:
           async with session.get(f'https://some-random-api.ml/binary?text={text}') as resp:
             resp = await resp.json()
@@ -60,7 +60,7 @@ class other(commands.Cog):
         
     @commands.command()
     @commands.cooldown(1,10,BucketType.user)
-    async def text(self, ctx, binary: str):
+    async def text(self, ctx, *, binary: str):
         async with aiohttp.ClientSession() as session:
           async with session.get(f'https://some-random-api.ml/binary?decode={binary}') as resp:
             resp = await resp.json()
