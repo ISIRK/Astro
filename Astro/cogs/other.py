ffa5b83d056d78257c9094c537c4d421336b8f4e
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-19 11:55:16 -0600

    Update other.py

--- Astro/cogs/other.py
+++ Astro/cogs/other.py
@@ -89,7 +89,7 @@ class other(commands.Cog):
             "`What was Halloween originally called?`": "ALL HALLOWS EVE",
             "`What was candy corn originally called?`": "CHICKEN FEED",
             "`(Approx)How much money does the average American spend on Halloween every year?`\n**A) $45\nB) $60\nC) $85\nD) $100**": "C",
-            "What percentage of kids like to recieve gum for halloween?": "10"
+            "`(Approx)What percentage of kids like to recieve gum for halloween?`": "10"
         }
         total_questions = len(qa)
         start_time = time.time()
