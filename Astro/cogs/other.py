30243f42fe603421a2ccb46722e587c15ba77536
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-19 15:52:04 -0600

    Update other.py

--- Astro/cogs/other.py
+++ Astro/cogs/other.py
@@ -111,7 +111,7 @@ class other(commands.Cog):
             resp = await resp.json()
                             
         embed=discord.Embed(title=f"Stats for {server}", description=f"IP: {resp['serverip']}\nStatus: {resp['serverStatus']}\nPing: {resp['ping']}\nVersion: {resp['version']}\nPlayers: {resp['players']}\nMax Players: {resp['maxplayers']}", color=0x2F3136)
-        embed.set_image(url=f"https://api.minetools.eu/favicon/{server}/25565")
+        embed.set_thumbnail(url=f"https://api.minetools.eu/favicon/{server}/25565")
         await ctx.send(embed=embed)
             
     @commands.command(aliases=['q'])
