d8f39f6f302a6adfc6fe8e1efaf559187ba1ccc4
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-10-24 13:15:58 -0600

    Update dev.py

--- Bot/cogs/dev.py
+++ Bot/cogs/dev.py
@@ -53,7 +53,7 @@ class dev(commands.Cog):
             await ctx.send(f'<:online:758139458767290421> Changed status to `Competing in {status}`')
         elif type == "streaming":
             await self.bot.change_presence(activity=discord.Streaming(name=f"{status}", url="https://www.twitch.tv/isirk"))
-            await ctx.send(f'<:online:758139458767290421> Changed status to `Streaming {status}`')
+            await ctx.send(f'<:streaming:769640090275151912> Changed status to `Streaming {status}`')
         elif type == "reset":
             await self.bot.change_presence(status=discord.Status.online)
             await ctx.send("<:online:758139458767290421> Reset Status")
