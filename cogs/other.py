bf9472594dc31b98229a764caa14997e3c181dcf
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-01 15:17:29 -0700

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -201,8 +201,8 @@ class other(commands.Cog):
             except asyncio.TimeoutError:
                 await ctx.send('Timeout Error')
             else:
-                embed = discord.Embed(title=f"{title}", description=f"{description}", color=0x2F3136)
-                await ctx.send(embed=embed)
+        embed = discord.Embed(title=f"{title}", description=f"{description}", color=0x2F3136)
+        await ctx.send(embed=embed)
 
 
 
