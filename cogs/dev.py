ffe7fc20205c49858f53b1836b417a43e6b6c322
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-01 15:06:06 -0700

    Update dev.py

--- cogs/dev.py
+++ cogs/dev.py
@@ -24,28 +24,64 @@ class dev(commands.Cog):
     '''Developer Commands'''
     def __init__(self, bot):
         self.bot = bot
+        
+        
+    @commands.is_owner()
+    @commands.command()
+    async def load(self, ctx, name: str):
+        """ Reloads an extension. """
+        try:
+            self.bot.load_extension(f"cogs.{name}")
+        except Exception as e:
+            return await ctx.send(f"```py\n{e}```")
+        await ctx.send(f"📥 Loaded extension **cogs/{name}.py**")
+
 
     @commands.is_owner()
     @commands.command()
-    async def load(self, ctx, extension):
-        '''Load a cog.'''
-        self.bot.load_extension(f'cogs.{extension}')
-        await ctx.send(f'{extension}' " loaded")
+    async def reload(self, ctx, name: str):
+        """ Reloads an extension. """
+
+        try:
+            self.bot.reload_extension(f"cogs.{name}")
+            await ctx.send(f"🔁 Reloaded extension **cogs/{name}.py**")
+
+        except Exception as e:
+            return await ctx.send(f"```py\n{e}```")
 
     @commands.is_owner()
     @commands.command()
-    async def unload(self, ctx, extension):
-        '''Unload a cog.'''
-        self.bot.unload_extension(f'cogs.{extension}')
-        await ctx.send(f'{extension}' " unloaded")
-
+    async def unload(self, ctx, name: str):
+        """ Reloads an extension. """
+        try:
+            self.bot.unload_extension(f"cogs.{name}")
+        except Exception as e:
+            return await ctx.send(f"```py\n{e}```")
+        await ctx.send(f"📤 Unloaded extension **cogs/{name}.py**")
+    
     @commands.is_owner()
     @commands.command()
-    async def reload(self, ctx, extension):
-        '''Reload a cog.'''
-        self.bot.unload_extension(f'cogs.{extension}')
-        self.bot.load_extension(f'cogs.{extension}')
-        await ctx.send(f':repeat: {extension}' " reloaded")
+    async def reloadall(self, ctx):
+        """ Reloads all extensions. """
+        error_collection = []
+        for file in os.listdir("cogs"):
+            if file.endswith(".py"):
+                name = file[:-3]
+                try:
+                    self.bot.reload_extension(f"cogs.{name}")
+                except Exception as e:
+                    error_collection.append(
+                        [file, default.traceback_maker(e, advance=False)]
+                    )
+
+        if error_collection:
+            output = "\n".join([f"**{g[0]}** ```diff\n- {g[1]}```" for g in error_collection])
+            return await ctx.send(
+                f"Attempted to reload all extensions, was able to reload, "
+                f"however the following failed...\n\n{output}"
+            )
+
+        await ctx.send("Successfully reloaded all extensions")
 
     @commands.is_owner()
     @commands.command()
