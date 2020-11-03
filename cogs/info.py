d5d7dc5cdd4ef124f230c0353d2837c23992b4f4
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-03 08:11:35 -0700

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -68,62 +68,6 @@ class info(commands.Cog):
         embed.set_footer(text=f"Use {ctx.prefix}help or info for more")
         await ctx.send(embed=embed)
         
-
-    @commands.command()
-    async def help1(self, ctx, *, page=None):
-        '''Outdated help command'''
-        helpembed1 = discord.Embed(title="Sirk", description="A Utilities Discord Bot with reliability and simplicity\n Made By [isirk#0001](https://discord.com/users/542405601255489537)", color=0x2F3136)
-        helpembed1.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-        helpembed1.add_field(name="Prefix", value="`@Astro ,Astro ,astro ,^`", inline=False)
-        helpembed1.add_field(name= "Help", value="React below to change the pages.\n**Note:**\nYou can type `help <page name>` to get a specific help page.", inline=False)
-        helpembed1.add_field(name="Pages", value="```yaml\n1) Info\n2) Utility\n3) Mod\n4) Other\n```", inline=False)
-        helpembed1.add_field(name="<a:settings:768181060734812230> About", value="Version 1.0 **Public Beta**\nMade with :heart: in <:python:758139554670313493>\nOwned, Developed, and Run by isirk#0001", inline=False)
-        helpembed1.set_footer(text="Sirk Bot | discord.gg/7yZqHfG")
-        
-        info = discord.Embed(title="Info", description="Information Commands", color=0x2F3136)
-        info.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-        info.add_field(name="Syntax:", value="```yaml\n<input> - Input for the Command\n[o] - Optional\n```", inline=False)
-        info.add_field(name="Commands:", value="`source` - See Sirks's Source Code\n`info` - Get Info About the Bot.\n`invite` - Get the invite for Astro.\n`support` Get Support Information.\n`privacy` - Privacy Policy for Astro.\n`credits` - See the credits for Astro.\n`help <[o]page name>` - This Message *or* a specific Help Page", inline=False)
-        info.set_footer(text="Sirk Bot | discord.gg/7yZqHfG")
-
-        utility = discord.Embed(title="Utility", description="Utiity Commands", color=0x2F3136)
-        utility.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-        utility.set_footer(text="Sirk Bot | discord.gg/7yZqHfG")
-        utility.add_field(name="Syntax:", value="```yaml\n<input> - Input for the Command\n[o] - Optional\n```", inline=False)
-        utility.add_field(name="Commands:", value="`avatar|av <[o]mention>` - Get the avatar of the mentioned user.\n`ping` - Get the Bot Ping.\n`server` - Get Info About The Server.\n`stats` - Get the Bot Stats.\n`user <[o]mention>` - Get the Stats of the Mentioned User.", inline=False)
-        
-        mod = discord.Embed(title="Mod", description="Moderation Commands\***Note you need specific permissions for each command.***", color=0x2F3136)
-        mod.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-        mod.set_footer(text="Sirk Bot | discord.gg/7yZqHfG")
-        mod.add_field(name="Syntax:", value="```yaml\n<input> - Input for the Command\n(permission) - Required Permissions```", inline=False)
-        mod.add_field(name="Commands:", value="`kick <user>` - Kick the mentioned user from the server. (`kick_members`)\n`ban <user>` - Ban the mentioned user from the server. (`ban_members`)\n`warn <user> <reason>` - Warn A User for a Specified Reason.(`kick_members`)\n`clear <#>` - Delete a number of messages. **Max 100** (`manage_messages`)\n`slowmode <seconds>` - Change the Channel Slowmode to a specified number of seconds. (`manage_channels`)\n~~~`mute <user>` - Mute a User.~~~ (Re-Working)\n~~~`unmute <user>` - Un-mutes a User.~~~ (Re-Working)", inline=False)
-        
-        other = discord.Embed(title="Other", description="Other Commands", color=0x2F3136)
-        other.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-        other.set_footer(text="Sirk Bot | discord.gg/7yZqHfG")
-        other.add_field(name="Commands:", value="`dice` - Roll A Dice (*There is a possibility you'll get a mystery.*)\n`joke` - Get a Joke\n`dadjoke` - Get a dadjoke\n`token` - Get a Fake Bot Token\n`binary <text>` - Turn text into Binary\n`text <binary>` - Turn Binary into Text\n`meme` - Get a Meme\n`programmerhumor|ph` - Get a Programming Meme\n`minecraft|mc <user>` - Get stats about a minecraft user.\n`minecraftserver|mcs <server ip>` - Get info about a Minecraft server\n`quiz|q` - Take the Halloween Quiz", inline=False)
-        
-        if page == "info":
-            await ctx.send(embed=info)
-        elif page == "utility":
-            await ctx.send(embed=utility)
-        elif page == "mod":
-            await ctx.send(embed=mod)
-        elif page == "other":
-            await ctx.send(embed=other)
-        else:
-            embeds = [
-            helpembed1,
-            info,
-            utility,
-            mod,
-            other,
-            Embed(title="End!", description="More to come...", color=discord.Colour.blurple())
-        ]
-
-        paginator = BotEmbedPaginator(ctx, embeds)
-        await paginator.run()
-        
     @commands.command()
     async def privacy(self, ctx):
         '''See the bots privacy policy'''
