c9860331377364f3d9e4cd206029811ea347c8ab
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-03 08:15:42 -0700

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -8,6 +8,8 @@ from discord.shard import ShardInfo
 
 import os
 
+import random
+
 import collections
 
 import time, datetime
@@ -21,6 +23,9 @@ class info(commands.Cog):
     '''Information Commands'''
     def __init__(self, bot):
         self.bot = bot
+    
+    listcolors = ['0x7289DA', '0x2F3136', 
+    COlORS = random.choice(listcolors)
         
     @commands.command()
     async def source(self, ctx):
@@ -30,7 +35,7 @@ class info(commands.Cog):
     @commands.command()
     async def about(self, ctx):
         '''Get information about the bot.'''
-        infoembed = discord.Embed(title="Info", description="A Utilities Discord Bot with reliability and simplicity", color=0x2F3136)
+        infoembed = discord.Embed(title="Info", description="A Utilities Discord Bot with reliability and simplicity", color=COLORS)
         infoembed.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         infoembed.add_field(name= "<:news:758781954073821194> News", value=f"**<:dev:759427919302492160> <@751447995270168586> Is Now PUBLIC! <:dev:759427919302492160>**\nTo invite Sirk click [here](https://discord.com/oauth2/authorize?client_id=751447995270168586&permissions=268823638&scope=bot).\nOr you can type `{ctx.prefix}invite` to get invite info!", inline=True)
         infoembed.set_thumbnail(url="https://cdn.discordapp.com/emojis/758451109919981580.png?v=1")
