3d86a5dade7717557f3357fdf264cbbfe94b3ddf
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-04 16:13:08 -0700

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -24,7 +24,7 @@ from disputils import BotEmbedPaginator, BotConfirmation, BotMultipleChoice
 colorfile = "/home/pi/Discord/Sirk/utils/prefixes.json"
 with open(colorfile) as f:
     data = json.load(f)
-colors = int(data['COLORS'], 16)
+color = int(data['COLORS'], 16)
 
 class info(commands.Cog):
     '''Information Commands'''
@@ -39,13 +39,13 @@ class info(commands.Cog):
     @commands.command()
     async def vote(self, ctx):
         '''Vote for Sirk Bot on top.gg'''
-        embed=discord.Embed(title="Vote", description="**Vote for Sirk Bot [here](https://top.gg/bot/751447995270168586/vote)**\nHave a cookie as well -> 🍪", color=colors)
+        embed=discord.Embed(title="Vote", description="**Vote for Sirk Bot [here](https://top.gg/bot/751447995270168586/vote)**\nHave a cookie as well -> 🍪", color=color)
         await ctx.send(embed=embed)
         
     @commands.command()
     async def about(self, ctx):
         '''Get information about the bot.'''
-        infoembed = discord.Embed(title="Info", description="A Utilities Discord Bot with reliability and simplicity", color=colors)
+        infoembed = discord.Embed(title="Info", description="A Utilities Discord Bot with reliability and simplicity", color=color)
         infoembed.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         infoembed.add_field(name= "<:news:758781954073821194> News", value=f"**<:dev:759427919302492160> <@751447995270168586> Is Now on [top.gg](https://top.gg)! <:dev:759427919302492160>**\nMake sure to go check Sirk's listing [here](https://top.gg/bot/751447995270168586)\nMake sure to also [vote](https://top.gg/bot/751447995270168586/vote)", inline=True)
         infoembed.set_thumbnail(url="https://cdn.discordapp.com/emojis/758451109919981580.png?v=1")
@@ -56,13 +56,13 @@ class info(commands.Cog):
     @commands.command()
     async def credits(self, ctx):
         '''See the credits for Sirk'''
-        embed = discord.Embed(title="Credits", description="**<@!542405601255489537> (isirk#0001) - Developer and Owner**\n**<@!555709231697756160> (CraziiAce#0001) - API Usage**\n**<@!668906205799907348> (Cyrus#8315) - Bot Optimizations**\n**<@!296862365503193098> (LeSirH#0001) - Optimizations and Advice**\n", color=colors)
+        embed = discord.Embed(title="Credits", description="**<@!542405601255489537> (isirk#0001) - Developer and Owner**\n**<@!555709231697756160> (CraziiAce#0001) - API Usage**\n**<@!668906205799907348> (Cyrus#8315) - Bot Optimizations**\n**<@!296862365503193098> (LeSirH#0001) - Optimizations and Advice**\n", color=color)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def support(self, ctx):
         '''Get support information.'''
-        supportembed = discord.Embed(title="Support", color=colors)
+        supportembed = discord.Embed(title="Support", color=color)
         supportembed.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         supportembed.set_thumbnail(url="https://cdn.discordapp.com/emojis/758453150897799172.png?v=1")
         supportembed.add_field(name="Support Server", value="<a:igloading:737723292768796713> Support Server: https://discord.gg/7yZqHfG", inline=False)
@@ -79,7 +79,7 @@ class info(commands.Cog):
     @commands.command()
     async def privacy(self, ctx):
         '''See the bots privacy policy'''
-        embed = discord.Embed(title="Privacy Policy for Sirk", description="Sirk strives to store no data to make Sirk as simple to use as possible.\nNothing is stored, recorded or anything of that sort.\n\n<:tab:758139554842148934> [Privacy Policy](https://asksirk.com/Astro/privacy/)", color=colors)
+        embed = discord.Embed(title="Privacy Policy for Sirk", description="Sirk strives to store no data to make Sirk as simple to use as possible.\nNothing is stored, recorded or anything of that sort.\n\n<:tab:758139554842148934> [Privacy Policy](https://asksirk.com/Astro/privacy/)", color=color)
         embed.set_author(name=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         embed.set_footer(text="Sirk Bot | discord.gg/7yZqHfG")
         await ctx.send(embed=embed)
