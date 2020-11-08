21fc8077228586ff02ac79bbebf99a2de7c928bb
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-08 13:52:41 -0700

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -52,8 +52,8 @@ class info(commands.Cog):
                 filename = code.co_filename
                 lines, firstline = inspect.getsourcelines(code)
                 location = os.path.relpath(filename).replace('\\', '/')
-                final_url = f'<{repo}/blob/master/{location}#L{firstline}-L>' \
-                            f'{firstline + len(lines) - 1}'
+                final_url = f'<{repo}/blob/master/{location}#L{firstline}-L' \
+                            f'{firstline + len(lines) - 1}>'
                 return await ctx.send(final_url)
         
     @commands.command()
