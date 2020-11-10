2160a52a64ea4ac679ee784cfed867e4c7c82b75
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-10 11:14:25 -0700

    Update disputils.py

--- cogs/disputils.py
+++ cogs/disputils.py
@@ -19,31 +19,31 @@ class disputils(commands.Cog):
      
      @commands.command()
      async def paginate(self, ctx):
-      embeds = [
-        Embed(title="test page 1", description="This is just some test content!", color=0x115599),
-        Embed(title="test page 2", description="Nothing interesting here.", color=0x5599ff),
-        Embed(title="test page 3", description="Why are you still here?", color=0x191638)
-      ]
+        embeds = [
+            Embed(title="test page 1", description="This is just some test content!", color=0x115599),
+            Embed(title="test page 2", description="Nothing interesting here.", color=0x5599ff),
+            Embed(title="test page 3", description="Why are you still here?", color=0x191638)
+        ]
 
-      paginator = BotEmbedPaginator(self, ctx, embeds)
-      await paginator.run()
+        paginator = BotEmbedPaginator(self, ctx, embeds)
+        await paginator.run()
     
     @commands.command()
     async def choice(self, ctx):
-      multiple_choice = BotMultipleChoice(ctx, ['one', 'two', 'three', 'four', 'five', 'six'], "Testing stuff")
-      await multiple_choice.run()
+        multiple_choice = BotMultipleChoice(ctx, ['one', 'two', 'three', 'four', 'five', 'six'], "Testing stuff")
+        await multiple_choice.run()
 
-      await multiple_choice.quit(multiple_choice.choice)
+        await multiple_choice.quit(multiple_choice.choice)
       
     @bot.command()
     async def confirm(ctx):
-      confirmation = BotConfirmation(ctx, 0x012345)
-      await confirmation.confirm("Are you sure?")
+        confirmation = BotConfirmation(ctx, 0x012345)
+        await confirmation.confirm("Are you sure?")
 
-      if confirmation.confirmed:
-          await confirmation.update("Confirmed", color=0x55ff55)
-      else:
-          await confirmation.update("Not confirmed", hide_author=True, color=0xff5555)
+        if confirmation.confirmed:
+            await confirmation.update("Confirmed", color=0x55ff55)
+        else:
+            await confirmation.update("Not confirmed", hide_author=True, color=0xff5555)
 
         
 def setup(bot):
