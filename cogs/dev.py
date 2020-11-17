4dcf2d771b4131f006abbaf2284a9fd908ca31fa
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-17 10:25:31 -0700

    Update dev.py

--- cogs/dev.py
+++ cogs/dev.py
@@ -170,7 +170,7 @@ class dev(commands.Cog):
     async def mystbin(self, data):
           data = bytes(data, 'utf-8')
           async with aiohttp.ClientSession() as cs:
-            async with cs.post('https://mystb.in/documents', data = data) as r:
+            async with cs.post('https://mystb.in/', data = data) as r:
               res = await r.json()
               key = res["key"]
               return f"https://mystb.in/{key}"
