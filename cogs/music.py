11ad0ec26c905b157ba97940994aff33fdcf3628
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-18 13:30:06 -0700

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -400,6 +400,8 @@ class music(commands.Cog):
             return await ctx.send('I am not currently playing anything!', delete_after=20)
 
         await self.cleanup(ctx.guild)
+        m = await self.cleanup()
+        await m.add_reaction(\U0000274c)
 
 
 def setup(bot):
