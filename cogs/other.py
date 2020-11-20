37ceebb6fb74e178a8dcb9ff9b10301b73ca19bb
Isaac Sirkin <67282396+ISIRK@users.noreply.github.com>
2020-11-20 11:42:18 -0700

    Update other.py

--- cogs/other.py
+++ cogs/other.py
@@ -7,12 +7,13 @@ from discord.shard import ShardInfo
 from discord.ext.commands import context
 from discord.ext.commands.cooldowns import BucketType
 
-import time, datetime
-from datetime import datetime
-
 import os
 
 import io
+
+import datetime
+
+import time
 
 import json
 
@@ -171,6 +172,37 @@ class other(commands.Cog):
                 return await ctx.send(f"Incorrect.\nIf you would like to try again type `{ctx.prefix}quest`")
         time_taken = time.time()- start_time
         await ctx.send(f"Correct!\nYou took **{time_taken:,.2f} seconds!**")
+
+    @commands.command(aliases=["weather", "cityweather"])
+    async def weathercity(self, ctx, *, city_name:str):
+        """Get the weather of a city/town by its name. State code is US only."""
+        # Code:
+        async with aiohttp.ClientSession() as session:
+            url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=168ced82a72953d81d018f75eec64aa0&units=imperial"
+            async with session.get(url) as response:
+                weather_response = await response.json()
+                if weather_response['cod'] != 200:
+                    await ctx.send(f"An error ocurred: `{weather_response['message']}`. If the issue persists, please contact a support team member with this error code: `{weather_response['cod']}`")
+                else:
+                    currentUnix = time.time()
+                    localSunrise = weather_response['sys']['sunrise'] + weather_response['timezone']
+                    sunriseTime = datetime.datetime.utcfromtimestamp(localSunrise)
+                    localSunset = weather_response['sys']['sunset'] + weather_response['timezone']
+                    sunsetTime = datetime.datetime.utcfromtimestamp(localSunset)
+                    localTimeUnix = currentUnix + weather_response['timezone']
+                    localTime = datetime.datetime.utcfromtimestamp(localTimeUnix)
+                    embed = discord.Embed(
+                        title=f"Weather in {weather_response['name']}, {weather_response['sys']['country']}",
+                        url = f"https://openweathermap.org/city/{weather_response['id']}",
+                        description=weather_response['weather'][0]['description'],
+                        color=color,
+                    )
+                    embed.add_field(name='Location:', value=f"**🏙️ City:** {weather_response['name']}\n**<:coordinates:727254888836235294> Longitude:** {weather_response['coord']['lon']}\n **<:coordinates:727254888836235294> Latitude:** {weather_response['coord']['lat']}", inline=False)
+                    embed.add_field(name='Weather', value=f"**🌡️ Current Temp:** {weather_response['main']['temp']}\n**🌡️ Feels Like:** {weather_response['main']['feels_like']}\n**🌡️ Daily High:** {weather_response['main']['temp_max']}\n**🌡️ Daily Low:** {weather_response['main']['temp_min']}\n**<:humidity:727253612778094683> Humidity:** {weather_response['main']['humidity']}%\n**🌬️ Wind:** {weather_response['wind']['speed']} mph", inline=False)
+                    embed.add_field(name='Time', value=f"**🕓 Local Time:** {localTime.strftime('%I:%M %p')}\n **🌅 Sunrise Time:** {sunriseTime.strftime('%I:%M %p')}\n **🌇 Sunset Time:** {sunsetTime.strftime('%I:%M %p')}")
+                    embed.set_thumbnail(url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
+                    embed.set_footer(text='Sirk Bot | discord.gg/7yZqHfG', icon_url=f"https://openweathermap.org/img/wn/{weather_response['weather'][0]['icon']}@2x.png")
+                    await ctx.send(embed=embed)
 
 
 
