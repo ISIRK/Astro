0a9e3820af1daa65522ad153a4fca4f1b929845c
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-11-21 10:23:15 -0700

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -14,11 +14,11 @@ from async_timeout import timeout
 from functools import partial
 from youtube_dl import YoutubeDL
 
-colorfile = "/home/pi/Discord/Sirk/utils/tools.json"
-with open(colorfile) as f:
+tools = "/home/pi/Discord/Sirk/utils/tools.json"
+with open(tools) as f:
     data = json.load(f)
 color = int(data['COLORS'], 16)
-
+footer = data['FOOTER']
 
 ytdlopts = {
     'format': 'bestaudio/best',
@@ -154,6 +154,7 @@ class MusicPlayer:
 
             self._guild.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
             embed=discord.Embed(title="Playing", description=f"Now playing `{source.title}`\n> Requested by `{source.requester}`", color=color)
+            embed.set_footer(text=footer)
             self.np = await self._channel.send(embed=embed)
             '''self.np = await self._channel.send(f'**Now Playing:** `{source.title}` requested by '
                                                f'`{source.requester}`')'''
@@ -341,7 +342,7 @@ class music(commands.Cog):
 
         fmt = '\n'.join(f'**`{_["title"]}`**' for _ in upcoming)
         embed = discord.Embed(title=f'Upcoming - Next {len(upcoming)}', description=fmt, color=color)
-
+        embed.set_footer(text=footer)
         await ctx.send(embed=embed)
 
     @commands.command(name='now_playing', aliases=['np'])
@@ -364,6 +365,7 @@ class music(commands.Cog):
         embed=discord.Embed(title="Now Playing", description=f"`{vc.source.title}`\n> Requested by {vc.source.requester}", color=color)
         '''player.np = await ctx.send(f'**Now Playing:** `{vc.source.title}` '
                                    f'requested by `{vc.source.requester}`')'''
+        embed.set_footer(text=footer)
         player.np = await ctx.send(embed=embed)
 
     @commands.command(name='volume', aliases=['vol'])
