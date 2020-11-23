fd5bca8c29cf11970fb9148098bfc98786f2963a
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-11-23 12:21:11 -0700

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -130,9 +130,9 @@ class utility(commands.Cog):
         perms = '\n'.join(perm for perm, value in member.guild_permissions if value)
 
         # And to make it look nice, we wrap it in an Embed.
-        embed = discord.Embed(title='Permissions for:', description=ctx.guild.name, colour=color)
+        embed = discord.Embed(title=f'Permissions for: {member}', colour=color)
         embed.set_author(icon_url=member.avatar_url, name=str(member))
-
+        embed.set_footer(text=footer)
         # \uFEFF is a Zero-Width Space, which basically allows us to have an empty field name.
         embed.add_field(name='\uFEFF', value=perms)
 
