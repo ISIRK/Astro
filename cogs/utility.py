cb1ce599c06e4dd3182e2122a3f04f44129fbe0f
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-11-30 16:59:13 -0700

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -23,7 +23,7 @@ with open(tools) as f:
 footer = data['FOOTER']
 color = int(data['COLOR'], 16)
 
-class utility(commands.Cog):
+class utility(commands.Cog, name="Utility"):
     '''Utility commands'''
     def __init__(self, bot):
         self.bot = bot
