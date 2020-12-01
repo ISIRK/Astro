87384c196017c02e108188a92d23d10901a76757
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-11-30 19:11:16 -0700

    Update error.py

--- cogs/error.py
+++ cogs/error.py
@@ -39,11 +39,11 @@ class ErrorHandler(Cog):
         elif isinstance(error, discord.NotFound): await ctx.send(embed = discord.Embed(title = str(error), color = discord.Color.red()))
         elif isinstance(error, commands.CommandOnCooldown): await ctx.send(embed = discord.Embed(title = str(error), color = discord.Color.red()))
         else:
-            c = self.bot.get_channel(783138336323403826) 
             embed = discord.Embed(
                 title = "An error occurred!",
                 description = f"Reported to the support server. Need more help? [Join the support server](https://discord.gg/7yZqHfG)\n```Error: \n{str(error)}```",
-                timestamp = datetime.datetime.utcnow()
+                timestamp = datetime.datetime.utcnow(),
+                color = discord.Color.red()
             )
             embed.set_footer(text = f"Caused by: {ctx.command}")
             await ctx.send(embed = embed)
@@ -52,13 +52,15 @@ class ErrorHandler(Cog):
             embed = discord.Embed(
                 title = f"An error occured!",
                 description = f"```{str(error)}```",
-                timestamp = datetime.datetime.utcnow()
+                timestamp = datetime.datetime.utcnow(),
+                color = discord.Color.red()
             )
+            c = self.bot.get_channel(783138336323403826) 
             embed.add_field(
                 name = "Details:",
                 value = f"""
                 Caused by: `{str(ctx.author)} [{int(ctx.author)}]`
-                In guild: `{str(ctx.guild)} [{int(ctx.guild)}]`
+                In guild: `{str(ctx.author)} [{ctx.author.id}]`
                 Command: `{ctx.command}`
                 """
             )
