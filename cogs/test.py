ba044bfdf0803763d1fc86b9d52ee36dd08735d7
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-11-30 17:08:35 -0700

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -1,12 +1,32 @@
 import discord, json
 import subprocess as sp
-from discord.ext import commands
+from discord.ext import commands, menus
 
 tools = "/home/pi/Discord/Sirk/utils/tools.json"
 with open(tools) as f:
     data = json.load(f)
 footer = data['FOOTER']
 color = int(data['COLOR'], 16)
+
+class MyMenu(menus.Menu):
+    async def send_initial_message(self, ctx, channel):
+        return await channel.send(f'Hello {ctx.author}')
+
+    @menus.button('\N{THUMBS UP SIGN}')
+    async def on_thumbs_up(self, payload):
+        await self.message.edit(content=f'Thanks {self.ctx.author}!')
+
+    @menus.button('\N{THUMBS DOWN SIGN}')
+    async def on_thumbs_down(self, payload):
+        await self.message.edit(content=f"That's not nice {self.ctx.author}...")
+
+    @menus.button('\N{BLACK SQUARE FOR STOP}\ufe0f')
+    async def on_stop(self, payload):
+        self.stop()
+
+class EmbedPageSource(menus.ListPageSource):
+    async def format_page(self, menu, embed):
+        return embed
 
 class test(commands.Cog, command_attrs=dict(hidden=True)):
     '''Testing Commands'''
@@ -30,7 +50,21 @@ class test(commands.Cog, command_attrs=dict(hidden=True)):
             pass
         msg = await ctx.send(embed=embed)
         for i in range(1, len(options) + 1): await msg.add_reaction(reactions[i])
-
+`   
+    @commands.command()
+    async def menu(self, ctx):
+        m = MyMenu()
+        await m.start(ctx)
+    
+    @commands.command()
+    async def emenu(self, ctx):
+        embeds = [
+            discord.Embed(title="Embed"),
+            discord.Embed(title="Another embed"),
+            discord.Embed(title="Some other embed")
+        ]
+        menu = menus.MenuPages(EmbedPageSource(embeds, per_page=1))
+        await menu.start(ctx)
         
 def setup(bot):
     bot.add_cog(test(bot))
