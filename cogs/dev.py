ce1657336c7a510d7d4b2af18c990fcba810ad23
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-12-01 14:47:56 -0700

    Update dev.py

--- cogs/dev.py
+++ cogs/dev.py
@@ -230,11 +230,6 @@ class dev(commands.Cog):
         embed = discord.Embed(title = "Active Cogs:", description = f"```yaml\n{s}```", color=color)
         embed.set_footer(text=footer)
         await ctx.send(embed=embed)
-
-    @commands.is_owner()
-    @commands.command()
-    async def replace(self, ctx, char, *, text):
-      await ctx.send(text.replace(" ", f" {char} "))
     
 def setup(bot):
     bot.add_cog(dev(bot))
