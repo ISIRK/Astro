46ceb1dd5679ce5c96bceff7140acce2ef586148
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-12-02 12:55:45 -0700

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -122,25 +122,5 @@ class mod(commands.Cog):
             await user.send(embed=embed)
             await ctx.send(f"<:help:758453150897799172> Warned {user}")
 
-    @commands.command(aliases=['em'])
-    @commands.has_permissions(manage_messages=True)
-    async def embed(self, ctx, channel: discord.TextChannel):
-        '''Make a custom embed and send it in any channel'''
-        await ctx.send("Embed Maker Started\nWhat would you like the title to be?")
-        try:
-            title = await self.bot.wait_for('message', timeout=60.0, check=lambda m:(ctx.author == m.author and ctx.channel == m.channel))
-        except asyncio.TimeoutError:
-            await ctx.send('Timeout Error')
-        else: 
-            await ctx.send("What would you like the description to be?")
-            try:
-                description = await self.bot.wait_for('message', timeout=60.0, check=lambda m:(ctx.author == m.author and ctx.channel == m.channel))
-            except asyncio.TimeoutError:
-                await ctx.send('Timeout Error')
-            else:
-                embed = discord.Embed(title=title.content, description=description.content, color=color)
-                await channel.send(embed=embed)
-                await ctx.send(f'`{title.content}` Embed sent in #{channel}')
-
 def setup(bot):
     bot.add_cog(mod(bot))
