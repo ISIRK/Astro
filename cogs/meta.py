e622e7a6ac973c5f2a6c73e71961ce89d2fd2845
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-12-02 13:45:26 -0700

    Update meta.py

--- cogs/meta.py
+++ cogs/meta.py
@@ -216,6 +216,44 @@ class meta(commands.Cog):
         embed.set_author(icon_url=member.avatar_url, name=str(member))
         embed.set_footer(text=footer)
         await ctx.send(content=None, embed=embed)
+
+    @commands.command()
+    async def source(self, ctx, *, command: str = None):
+        """Displays my full source code or for a specific command.
+        To display the source code of a subcommand you can separate it by
+        periods
+        """
+        source_url = 'https://github.com/isirk/Sirk'
+        branch = 'master'
+        if command is None:
+            return await ctx.send(source_url)
+
+        if command == 'help':
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
+        else:
+            obj = self.bot.get_command(command.replace('.', ' '))
+            if obj is None:
+                return await ctx.send('Could not find command.')
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+        if not module.startswith('discord'):
+            # not a built-in command
+            location = os.path.relpath(filename).replace('\\', '/')
+        else:
+            location = module.replace('.', '/') + '.py'
+            source_url = 'https://github.com/Rapptz/discord.py'
+            branch = 'master'
+
+        final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
+        await ctx.send(final_url)
                         
 def setup(bot):
     bot.add_cog(meta(bot))
