b74b5767bdb8a917bdfa56cb7cf7d2e77cbcecb2
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-12-04 09:06:10 -0700

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -210,9 +210,12 @@ class misc(commands.Cog):
                     embed.set_thumbnail(url=activity.album_cover_url)
                     embed.add_field(name="Artist", value=activity.artist)
                     embed.add_field(name="Album", value=activity.album)
+                    embed.add_field(name="Duration", value=duration, inline=False)
                     embed.add_field(name="Elapsed", value="{}".format(activity.created_at.strftime("%H:%M")), inline=False)
                     embed.set_footer(text=footer)
                     await ctx.send(embed=embed)
+                else:
+                    await ctx.send('No Spotify Activity Found')
 
 def setup(bot):
     bot.add_cog(misc(bot))
