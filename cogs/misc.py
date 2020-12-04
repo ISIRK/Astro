ef364cfb684131a5bec3178e86545ef416daaae7
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-12-04 08:27:39 -0700

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -197,21 +197,22 @@ class misc(commands.Cog):
 
     @commands.command()
     async def spotify(self, ctx, user: discord.Member = None):
-    if user == None:
-        user = ctx.author
-        await ctx.send('No spotify Activity')
-    if user.activities:
-        for activity in user.activities:
-            if isinstance(activity, Spotify):
-                embed = discord.Embed(
-                    title = f"{user.name}'s Spotify",
-                    description = "Listening to {}".format(activity.title),
-                    color = 0xC902FF)
-                embed.set_thumbnail(url=activity.album_cover_url)
-                embed.add_field(name="Artist", value=activity.artist)
-                embed.add_field(name="Album", value=activity.album)
-                embed.set_footer(text="Song started at {}".format(activity.created_at.strftime("%H:%M")))
-                await ctx.send(embed=embed)
+        '''Returns info about spotify playback.'''
+        if user == None:
+            user = ctx.author
+            await ctx.send('No spotify Activity')
+        if user.activities:
+            for activity in user.activities:
+                if isinstance(activity, Spotify):
+                    embed = discord.Embed(
+                        title = f"{user.name}'s Spotify",
+                        description = "Listening to {}".format(activity.title),
+                        color = 0xC902FF)
+                    embed.set_thumbnail(url=activity.album_cover_url)
+                    embed.add_field(name="Artist", value=activity.artist)
+                    embed.add_field(name="Album", value=activity.album)
+                    embed.set_footer(text="Song started at {}".format(activity.created_at.strftime("%H:%M")))
+                    await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(misc(bot))
