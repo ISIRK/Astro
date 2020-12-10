25771ef55a0a357e7362fe9fd99946d7fc9c7d46
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-12-10 13:02:08 -0700

    Update dev.py

--- cogs/dev.py
+++ cogs/dev.py
@@ -237,7 +237,7 @@ class dev(commands.Cog):
             await ctx.send(f'<:online:758139458767290421> Changed status to `Watching {len(self.bot.users)} users`')
 
     @status.command()
-    async def reset(self, ctx, *, status: str):
+    async def reset(self, ctx):
         await self.bot.change_presence(status=discord.Status.online)
         await ctx.send("<:online:758139458767290421> Reset Status")
         
