432d8279cc1df2c33adcc1fa04b9422beebb1716
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-12-14 10:10:24 -0700

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -66,14 +66,14 @@ class test(commands.Cog, command_attrs=dict(hidden=True)):
 
     def check_if_string_in_file(file_name, string_to_search):
     """ Check if any line in the file contains given string """
-    # Open the file in read only mode
-    with open(file_name, 'r') as read_obj:
-        # Read all lines in the file one by one
-        for line in read_obj:
-            # For each line, check if line contains the string
-            if string_to_search in line:
-                return True
-    return False
+        # Open the file in read only mode
+        with open(file_name, 'r') as read_obj:
+            # Read all lines in the file one by one
+            for line in read_obj:
+                # For each line, check if line contains the string
+                if string_to_search in line:
+                    return True
+        return False
     
     @commands.command()
     async def emenu(self, ctx):
