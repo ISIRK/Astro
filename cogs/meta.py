04f3c68323271dc29d64f2546a3363f41c7467fe
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-12-17 08:17:50 -0700

    Update meta.py

--- cogs/meta.py
+++ cogs/meta.py
@@ -32,7 +32,6 @@ from discord.utils import get
 import os
 import collections
 import time, datetime
-from datetime import datetime
 from discord.ext.commands.cooldowns import BucketType
 import random
 import psutil
@@ -126,14 +125,15 @@ class meta(commands.Cog):
         '''Get the bot ping'''
         await ctx.message.add_reaction('🏓')
         await ctx.send(f"Pong! Average Latency is {round(self.bot.latency * 1000)} ms")
-
+        
     @commands.command()
     async def uptime(self, ctx):
-        delta_uptime = datetime.utcnow() - self.bot.launch_time
+        """Displays the uptime"""
+        delta_uptime = datetime.datetime.utcnow() - self.bot.start_time
         hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
         minutes, seconds = divmod(remainder, 60)
         days, hours = divmod(hours, 24)
-        await ctx.send(f"Been up for {days}d, {hours}h, {minutes}m, {seconds}s")
+        await ctx.send(f"Been up for {days}d, {hours}h, {minutes}m, {seconds}s"))
 
     @commands.command()
     async def server(self, ctx):
