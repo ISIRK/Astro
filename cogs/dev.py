fc498bc335d81d920ea7b4bce2453089adabefbb
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2020-12-31 17:56:45 -0700

    Update dev.py

--- cogs/dev.py
+++ cogs/dev.py
@@ -83,7 +83,7 @@ class dev(commands.Cog):
 
         try:
             self.bot.reload_extension(f"cogs.{name}")
-            await ctx.message.add_reactions('🔄')
+            await ctx.message.add_reaction('🔄')
 
         except Exception as e:
             return await ctx.send(f"```py\n{e}```")
