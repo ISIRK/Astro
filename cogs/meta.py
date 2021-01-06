3b30c6003a3ce2945876e3469f48afe41471f0f6
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-01-06 15:32:56 -0700

    Update meta.py

--- cogs/meta.py
+++ cogs/meta.py
@@ -57,7 +57,7 @@ class meta(commands.Cog):
         
     @commands.Cog.listener()
     async def on_message(self, message):
-        if "invite" in message.content and "sirk".lower() in message.content:
+        if "invite".lower() in message.content and "sirk".lower() in message.content:
             await message.channel.send('<https://discord.com/oauth2/authorize?client_id=751447995270168586&permissions=268823638&scope=bot>')
             
 
