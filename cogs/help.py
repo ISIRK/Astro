2c3a7efb17a7bc1b437c10338940b40cb2e81b28
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-01-22 11:27:49 -0700

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -37,7 +37,7 @@ class HelpCommand(commands.HelpCommand):
         await self.get_destination().send(embed=embed)
 
     async def send_command_help(self, command):
-        use = await command.can_run(self.context)
+        #use = await command.can_run(self.context)
         embed = discord.Embed(title=command.name,
                               description=command.help or "No info available.",
                               colour=self.context.bot.color)
