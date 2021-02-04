9d54d62fff293a5d02983829c9ec104f16b77e70
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-02-04 13:52:25 -0700

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -273,6 +273,7 @@ class music(commands.Cog):
 
         # If download is False, source will be a dict which will be used later to regather the stream.
         # If download is True, source will be a discord.FFmpegPCMAudio with a VolumeTransformer.
+        await ctx.send(f'<:youtube:737723983335784458> Searching for **{search}** on youtube.')
         source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop, download=False)
 
         await player.queue.put(source)
