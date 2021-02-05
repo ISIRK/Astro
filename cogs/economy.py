a23d1b25906d94d3ee1a5bc2656d01a10f38aef7
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-02-05 14:56:57 -0700

    Create economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -258,7 +258,9 @@ class economy(commands.Cog):
             inv = ['Multiplier']
             try:
                 items = a['inv'].append(inv[product])
+                await ctx.send(f'{items}')
                 await self.bot.db.execute("UPDATE economy SET inv = $1 WHERE userId = $2", items, ctx.author.id)
+                await ctx.send(f'Successfully bought **{inv[product]}**')
             except Exception as e:
                 await ctx.send(f'```py\n{e}```')
                                
