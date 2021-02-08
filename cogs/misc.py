85d8501048bf05dbe8209c2158fdb2ea6d901cff
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-02-08 16:59:56 -0700

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -214,5 +214,21 @@ class misc(commands.Cog):
     async def bossbadi(self, ctx):
         await ctx.reply('bossbadi is a cool dude and a great bot dev')
 
+    @commands.command()
+    @commands.cooldown(1,3,BucketType.user)
+    @commands.bot_has_permissions(manage_webhooks=True)
+    async def hook(self,ctx, *, words):
+        '''Make a custom webhook with your avatar, name, and a bot tag.'''
+        channel = ctx.channel
+        i = await channel.create_webhook(name="Hook")
+        url = i.url
+        await ctx.message.delete()
+        try:
+            webhook = Webhook.from_url(str(url), adapter=AsyncWebhookAdapter(self.bot.session))
+            await webhook.send(words, username=ctx.author.name, avatar_url=ctx.author.avatar_url)
+            return
+        except:
+            pass
+
 def setup(bot):
     bot.add_cog(misc(bot))
