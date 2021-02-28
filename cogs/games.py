68c8fa5b6e58ab76e08e3d6e359638a684571466
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-02-28 16:06:41 -0700

    Update games.py

--- cogs/games.py
+++ cogs/games.py
@@ -62,7 +62,7 @@ class BasketballMenu(menus.Menu):
     async def do_shot(self, _):
         if self.score >= 21:
             await self.message.edit(embed=discord.Embed(title='You reached 21!', description="Good job. Thats it for now. Come play again later.", color=self.ctx.bot.color))
-            self.clear_buttons(*, react=True)
+            self.clear_buttons(react=True)
         else:
             lucky = random.choice([True, False])
             if lucky:
