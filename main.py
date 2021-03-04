298182d41418837d7aca6acc7303d79a6021c26f
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-03-04 11:54:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ token = data['TOKEN']
 bot = Sirk()
 
 os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
-
-# also
 os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
 os.environ["JISHAKU_HIDE"] = "True"
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+if __name__ == "__main__":
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
 
-bot.load_extension("jishaku")
+    bot.load_extension("jishaku")
 
-bot.run(token)
+    bot.run(token)
