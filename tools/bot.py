55fad7408768a8b0df7efbc8c327d3670a22f77e
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-03-04 17:50:00 -0700

    Update bot.py

--- tools/bot.py
+++ tools/bot.py
@@ -88,7 +88,7 @@ class Context(commands.Context):
         m = await self.send(*args, **kwargs)
         await m.add_reaction('🇽')
         try:
-            await self.bot.wait_for('reaction_add', check=lambda r, u: u.id == self.author.id and r.message.id == m.id and str(r.emoji) == '❌')
+            await self.bot.wait_for('reaction_add', check=lambda r, u: u.id == self.author.id and r.message.id == m.id and str(r.emoji) == '🇽')
             await m.delete()
         except asyncio.TimeoutError:
             pass
