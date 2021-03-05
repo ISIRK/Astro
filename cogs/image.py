9b979b6097441bbc28e59f586d74edbd6dda43a5
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-03-05 10:42:36 -0700

    Update image.py

--- cogs/image.py
+++ cogs/image.py
@@ -278,7 +278,7 @@ class image(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 15, co
         file=discord.File(buffer, filename="ascii.png")
         e=discord.Embed(color=self.invis)
         e.set_author(name="Ascii Avatar", icon_url=member.avatar_url)
-        e.set_image(url="attachment://ascii.gif")
+        e.set_image(url="attachment://ascii.png")
         await ctx.remove(file=file, embed=e)
 
 def setup(bot):
