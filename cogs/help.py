a042f9493d14d4a20387807b50a0234ad3530225
isirk <67282396+isirk@users.noreply.github.com>
2021-03-08 17:24:51 -0700

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -11,14 +11,19 @@ class HelpCommand(commands.HelpCommand):
         })
 
     async def send_bot_help(self, mapping):
-        embed = discord.Embed(title='Command List', description=self.context.bot.description, url="https://asksirk.com/bot/commands", colour=self.context.bot.color)
+        embed = discord.Embed(title='Help', description=self.context.bot.description, url="https://asksirk.com/bot/commands", colour=self.context.bot.color)
         
+        '''
         for cog, commands in mapping.items():
             name = 'No Category' if cog is None else cog.qualified_name
             filtered = await self.filter_commands(commands, sort=True)
             if filtered:
                 value = ' '.join(f'`{c.name}`' for c in commands)
                 embed.add_field(name=name, value=value, inline=False)
+        '''
+        for cog in mapping.items():
+            name = cog.qualified_name
+            embed.add_field(name=name, value=f'```{self.clean_prefix}help {name}```')
 
         embed.set_footer(text='Use {0}{1} [command|module] for more info.'.format(self.clean_prefix, self.invoked_with))#self.get_ending_note())
         await self.get_destination().send(embed=embed)
