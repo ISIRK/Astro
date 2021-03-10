575d1e05a6b8380e9f3b229a7bd85c7e7ba25fad
isirk <67282396+isirk@users.noreply.github.com>
2021-03-10 09:48:01 -0700

    Update image.py

--- cogs/image.py
+++ cogs/image.py
@@ -119,7 +119,7 @@ class image(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, co
             gif = Image.open(gif)
             wash = Image.open("cogs/assets/wash.png")
             frames = []
-            for i in range(30):
+            for frame in ImageSequence(gif):
                 frame = frame.copy()
                 frame.paste(wash) #, mask=wash
                 frames.append(frame)
