12dd05576894937286e77bee4a05739e2856e71c
isirk <67282396+isirk@users.noreply.github.com>
2021-03-09 17:46:27 -0700

    Update image.py

--- cogs/image.py
+++ cogs/image.py
@@ -361,6 +361,22 @@ class image(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, co
         await ctx.remove(file=file, embed=e)
 
     @commands.command()
+    async def cube(self, ctx, *, member: discord.Member = None):
+        '''Cube the avatar'''
+        if not member:
+            member = ctx.author
+        url = member.avatar_url_as(size=512, format="png")
+        async with ctx.typing():
+            img = BytesIO(await url.read())
+            img.seek(0)
+            buffer = await self.bot.loop.run_in_executor(None, self.do_cube, img)
+        file=discord.File(buffer, filename="cube.png")
+        e=discord.Embed(color=self.invis)
+        e.set_author(name="Cubed Avatar", icon_url=member.avatar_url)
+        e.set_image(url="attachment://cube.png")
+        await ctx.remove(file=file, embed=e)
+
+    @commands.command()
     async def sketch(self, ctx, *, member: discord.Member = None):
         '''Sketches the avatar'''
         if not member:
