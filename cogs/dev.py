70f37462fecdf345face98dc7ca2bd6d4ffb1aef
isirk <67282396+isirk@users.noreply.github.com>
2021-03-15 15:17:13 -0600

    Update dev.py

--- cogs/dev.py
+++ cogs/dev.py
@@ -165,7 +165,7 @@ class dev(commands.Cog):
         def is_me(m):
             return m.author == self.bot.user
 
-        deleted = await channel.purge(limit=limit, check=is_me, bulk=False)
+        deleted = await channel.purge(limit=limit+1, check=is_me, bulk=False)
 
     @commands.command()
     async def edit(self, ctx, id: int, *, content):
