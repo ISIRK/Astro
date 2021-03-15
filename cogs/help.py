03b6569f9cceae3201ce4146e5e0ce88eb2e93e0
isirk <67282396+isirk@users.noreply.github.com>
2021-03-15 16:26:18 -0600

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -12,23 +12,13 @@ class HelpCommand(commands.HelpCommand):
 
     async def send_bot_help(self, mapping):
         embed = discord.Embed(title='Help', description=self.context.bot.description, url="https://asksirk.com/bot/commands", colour=self.context.bot.color)
-        
-        '''
-        for cog, commands in mapping.items():
-            name = 'No Category' if cog is None else cog.qualified_name
-            filtered = await self.filter_commands(commands, sort=True)
-            if filtered:
-                value = ' '.join(f'`{c.name}`' for c in commands)
-                embed.add_field(name=name, value=value, inline=False)
-        '''
         for cog, commands in mapping.items():
             if cog is None:
                 pass
             else:
                 filtered = await self.filter_commands(commands, sort=True)
                 if filtered:
-                    name = cog.qualified_name
-                    embed.add_field(name=name.capitalize(), value=f'```{self.clean_prefix}{self.invoked_with} {name}```')
+                    embed.add_field(name=cog.qualified_name.capitalize(), value=f'```{self.clean_prefix}{self.invoked_with} {name}```')
 
         embed.set_footer(text='Use {0}{1} [command|module] for more info.'.format(self.clean_prefix, self.invoked_with))#self.get_ending_note())
         return await self.context.send(embed=embed)
@@ -61,9 +51,6 @@ class HelpCommand(commands.HelpCommand):
         embed = discord.Embed(title=group.name,
                               description=group.help or "No info available.",
                               colour=self.context.bot.color)
-        '''
-        embed.add_field(name="Usage:", value=f"{group.name} {group.signature}", inline=False)
-        '''
         if group.aliases:
             embed.add_field(name="Aliases:", value="\n".join(group.aliases), inline=False)
         embed.add_field(name="Commands:", value="\n".join(f"**{command}** - {command.short_doc}" for command in group.walk_commands()) or "None")
