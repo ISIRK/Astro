5de776d398af453619de7309a7b6fd44d13f3162
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-03-29 16:30:25 -0600

    Update dev.py

--- cogs/dev.py
+++ cogs/dev.py
@@ -232,11 +232,13 @@ class dev(commands.Cog):
     @todo.command()
     async def add(self, ctx, *, thing:str):
         '''Add something to the todo list'''
-        try:
-            await self.bot.db.execute("INSERT INTO todo VALUES ($1)", thing)
-            await ctx.send(f'Added {thing} to your todo list!')
-        except Exception as e:
-            return await ctx.send(e)
+        s = await self.bot.db.fetch("SELECT things FROM todo WHERE id = $1", ctx.author.id)
+        if s:
+            try:
+                await self.bot.db.execute("INSERT INTO todo(things) VALUES ($1) WHERE id = $2", s['things'].append(thing), ctx.author.id)
+                await ctx.send(f'Added {thing} to your todo list!')
+            except Exception as e:
+                return await ctx.send(e)
 
     @todo.command(aliases=['remove'])
     async def delete(self, ctx, *, thing:str):
