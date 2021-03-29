b14d85636e0100598b4f8864c4351913d7ba55f2
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-03-29 17:01:38 -0600

    Update dev.py

--- cogs/dev.py
+++ cogs/dev.py
@@ -221,12 +221,10 @@ class dev(commands.Cog):
     @commands.group(invoke_without_command=True)
     async def todo(self, ctx):
         """Todo Commands"""
-        s = await self.bot.db.fetch("SELECT * FROM todo WHERE id = $1", ctx.author.id)
+        s = await self.bot.db.fetch("SELECT things FROM todo WHERE id = $1", ctx.author.id)
         if s:
             try:
-                list = s['things']
-                entries = [item for item in list]
-                p = self.bot.utils.SimpleMenu(entries=list, per_page=10)
+                p = self.bot.utils.SimpleMenu(entries=s, per_page=10)
                 await p.start(ctx)
             except Exception as e:
                 await ctx.send(f'{e}')
