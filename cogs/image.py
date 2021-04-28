5f0c358d1d38ff92653f20407f3952d6a8257e93
Isaac Sirkin <67282396+isirk@users.noreply.github.com>
2021-04-28 04:24:39 -0600

    Update image.py

--- cogs/image.py
+++ cogs/image.py
@@ -426,8 +426,8 @@ class image(commands.Cog, command_attrs={'cooldown': commands.Cooldown(1, 10, co
             else:
                 if m.content == resp['content']:
                     end = datetime.datetime.now()
-                    time = humanize.precisedelta((end - start).total_seconds())
-                    await ctx.send(f"{m.author.mention} typed it correct in `{time}` with **{time/len(resp['content'])/5}** wpm.")
+                    time = end - start
+                    await ctx.send(f"{m.author.mention} typed it correct in `{humanize.precisedelta((end - start).total_seconds())}` with **{time/len(resp['content'])/5}** wpm.")
                     going = False
 
     @commands.command()
